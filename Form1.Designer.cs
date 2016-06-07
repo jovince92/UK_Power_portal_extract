d9bf72b17de36cdf4017db98c4a5342408ce2508
Vince <vince.sanity123@gmail.com>
2016-06-08 00:45:17 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(791, 187);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1016, 187);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 239);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(1016, 113);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 358);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1016, 131);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 501);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1{
+    public partial class Form1 : Form{
+        public Form1(){
+            InitializeComponent();
+        }
+
+        static string result;
+
+        static string tmp = @"C:\UK POWER\uk_power_" + DateTime.Now.ToString("ddMMyyyy") + ".csv";
+        static string header = @"Reference,Affiliate tracking ID,Date,Supplier,Title,First Name,Last Name,Postcode,Fuel,Campaign,Referrer code,Gas Status,Elec Status";
+        static System.Globalization.DateTimeFormatInfo monthInfo = new System.Globalization.DateTimeFormatInfo();     
+        
+        private void button1_Click(object sender, EventArgs e){            
+            string[] lines = this.richTextBox1.Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            ArrayList formattedArr = new ArrayList();
+            formattedArr = reformatArr(lines);
+            foreach (string tabs in formattedArr) {
+                string[] fields = tabs.Split('\t');
+                string[] parts = fields[2].Split('/');
+                string dt = parts[0]+" "+monthInfo.GetMonthName( Int32.Parse( parts[1]) )+" "+parts[2];
+                string[] names = fields[4].Split(' ');
+                result = result + fields[0] + ",";
+                result = result + fields[1] + ",";
+                result = result + dt        + ",";
+                result = result + fields[3] + ",";
+                result = result + names[0]  + ",";
+                result = result + names[1]  + ",";
+                result = result + names[2]  + ",";
+                result = result + fields[5] + ",";
+                result = result + fields[6] + ",";
+                result = result + fields[7] + ",";
+                result = result + fields[8] + ",";
+                result = result + fields[9] + ",";
+                result = result + fields[10];
+                /*--------------------------------------*/
+                var subParts = result.Split(',');
+                ListViewItem lvi = new ListViewItem(subParts[0]);
+                foreach (string listRows in subParts.Skip(1)) {
+                    lvi.SubItems.Add(listRows);
+                }
+                listView1.Items.Add(lvi);                
+                richTextBox2.Text = richTextBox2.Text + result+Environment.NewLine;
+                result = "";                
+            }            
+        }
+
+        private void button2_Click(object sender, EventArgs e){
+            System.IO.Directory.CreateDirectory(@"c:\UK POWER\");            
+            System.IO.File.WriteAllText(tmp, richTextBox2.Text);
+            System.Diagnostics.Process.Start(@"C:\UK POWER\");            
+        }
+
+        public System.Collections.ArrayList reformatArr(string[] lines) {
+            ArrayList returnThis = new ArrayList();
+            for (int i = 0; i < lines.Length;i++ ) {
+                if (!lines[i].Contains("Reference")) {
+                    returnThis.Add(lines[i]);
+                }
+            }
+            return returnThis;
+        }
+
+        private void Form1_Load(object sender, EventArgs e){            
+            string[] Rcolumns = header.Split(',');
+            listView1.View = View.Details;
+            for (int i = 0; i < Rcolumns.Length; i++) {
+                listView1.Columns.Add(Rcolumns[i]).Width = 81;                
+            }
+            richTextBox2.Text = header + Environment.NewLine;
+        }
+    }
+}
